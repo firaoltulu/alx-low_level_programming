b691d1a154bc262231c4f42cedc71d57bd3d59c7
firaoltulu <firaoltulu5@gmail.com>
2023-05-18 01:27:38 -0700

    my sixth commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry function
+*
+* @ac: used to accept values from shell command
+* @av: used to accept values from shell command
+*
+* Return: 1
+*/
+int main(int ac, char **av)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
