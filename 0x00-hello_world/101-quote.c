42d76901a62a857c27e2a5722209c8f1391531f6
firaoltulu <firaoltulu5@gmail.com>
2023-05-18 01:30:21 -0700

    my seventh commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,10 @@
 /**
 * main - Entry function
 *
-* @ac: used to accept values from shell command
-* @av: used to accept values from shell command
 *
 * Return: 1
 */
-int main(int ac, char **av)
+int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
