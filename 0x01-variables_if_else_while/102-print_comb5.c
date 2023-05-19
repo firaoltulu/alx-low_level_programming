be24ef4b4346edbb7a9c634bb17dc9af0dbd3740
firaoltulu <firaoltulu5@gmail.com>
2023-05-19 05:45:15 -0700

    my fivtheen commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Function
+ * Return: 0
+ */
+int main(void)
+{
+int four = 0, five;
+do {
+for (five = 0; five < 100; five++)
+{
+if (four < five)
+{
+putchar((four / 10) + 48);
+putchar((four % 10) + 48);
+putchar(' ');
+putchar((five / 10) + 48);
+putchar((five % 10) + 48);
+if (four != 98 || five != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+four++;
+} while (four < 100);
+return (0);
+}
