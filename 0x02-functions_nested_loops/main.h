1fec046211613e632bfa00264e7e60ef322e8512
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 04:38:18 -0700

    my second commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+int print_last_digit(int);
+void jack_bauer(void);
+void print_to_98(int n);
+void times_table(void);
+int _putchar(char);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+void print_alphabet(void);
+int add(int, int);
+void print_alphabet_x10(void);
+int _islower(int c);
+void print_times_table(int n);
+
+#endif
