ea696c5eef2863ce548fc93d6d826e7f52d107a8
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 06:42:50 -0700

    my eigtheen commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * fir_sum - Calculate the sum of the even-valued terms
+ * of fibonacci number
+ *
+ * Return: sum
+ */
+int fir_sum(void)
+{
+int ind;
+unsigned long int ten = 1, nine = 2;
+unsigned long int next, sum = 0;
+
+for (ind = 1; ind <= 33; ++ind)
+{
+if (ten < 4000000 && (ten % 2) == 0)
+{
+sum = sum + ten;
+}
+next = ten + nine;
+ten = nine;
+nine = next;
+}
+}
+
+/**
+ * main - Entry function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+int sum = fir_sum();
+
+printf("%lu\n", sum);
+
+return (0);
+}
