11bb854513737ebfb16ba31195086e882c655f62
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 05:07:48 -0700

    my fivth commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,18 +12,20 @@
 
 int print_sign(int n)
 {
-	switch (n)
-	{
-		case n > 0:
+
+		if (n > 0)
+		{
 			_putchar('+');
 			return (1);
-		case n == 0:
+		}
+		else if (n == 0)
+		{
 			_putchar('0');
 			return (0);
-		case n < 0:
+		}
+		else if (n < 0)
+		{
 			_putchar('-');
 			return (-1);
-		default:
-		return (2);
-	}
+		}
 }
