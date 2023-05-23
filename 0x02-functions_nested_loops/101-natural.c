9901918655f6f2a1a3001e1eabfab54eb8f6e3f6
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 06:16:25 -0700

    my fivtheen commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry function
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+int sum = fir_sum();
+
+printf("%d\n", sum);
+
+return (0);
+}
+
+/**
+ * fir_sum - adds the number that are divide able by 3 & 5 between 0 & 1024
+ *
+ * Return: the sum.
+ */
+
+int fir_sum(void)
+{
+int two = 0, sum = 0;
+
+do {
+if ((two % 3) == 0 || (two % 5) == 0)
+sum += two;
+
+two++;
+} while (two < 1024);
+
+return (sum);
+}
+
