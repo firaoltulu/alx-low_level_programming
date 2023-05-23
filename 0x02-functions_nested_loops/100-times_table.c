36e1e1b116960c757a67e9de2597731c9d049189
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 11:55:13 -0700

    my second commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,25 +1,18 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
+ * fir_check - prints
  *
- * @n: Is an int that is passed to this function from
- * the caller it is number of the times table
+ * @two: Is an int that is passed to this function from
+ * the caller
+ * @k: Is an int that is passed to this function from
+ * the caller
  *
  * return: nothing (void)
  */
 
-void print_times_table(int n)
+void fir_check(int two, int k)
 {
-int one, two, k;
-
-if (n >= 0 && n <= 15)
-{
-for (one = 0; one <= n; one++)
-{
-for (two = 0; two <= n; two++)
-{
-k = two * one;
 if (two == 0)
 {
 _putchar(k + '0');
@@ -48,6 +41,29 @@ _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
 }
+}
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * @n: Is an int that is passed to this function from
+ * the caller it is number of the times table
+ *
+ * return: nothing (void)
+ */
+
+void print_times_table(int n)
+{
+int one, two, multi;
+
+if (n >= 0 && n <= 15)
+{
+for (one = 0; one <= n; one++)
+{
+for (two = 0; two <= n; two++)
+{
+multi = two * one;
+fir_check(two, multi);
 }
 _putchar('\n');
 }
