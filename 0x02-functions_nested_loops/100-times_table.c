78b58c6365d9cdc1fe94c254a67cffe85bae004e
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 06:00:08 -0700

    my thirteen commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,53 +20,36 @@ for (one = 0; one <= n; one++)
 for (two = 0; two <= n; two++)
 {
 multi = two * one;
-fir_check(one, two, multi);
+if (two == 0)
+{
+_putchar(k + '0');
+}
+else if (k < 10 && two != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
+_putchar((k % 10) + '0');
+}
+else if (k >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
+_putchar((k % 10) + '0');
+}
 }
 _putchar('\n');
 }
 }
 }
-/**
- * fir_check - prints
- *
- * @one: Is an int that is passed to this function from
- * the caller
- * @two: Is an int that is passed to this function from
- * the caller
- * @k: Is an int that is passed to this function from
- * the caller
- *
- * return: nothing (void)
- */
-void fir_check(int one, int two, int k)
-{
-	if (two == 0)
-	{
-		_putchar(k + '0');
-	}
-	else if (k < 10 && two != 0)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar(k + '0');
-	}
-	else if (k >= 10 && k < 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar(' ');
-		_putchar((k / 10) + '0');
-		_putchar((k % 10) + '0');
-	}
-	else if (k >= 100)
-	{
-		_putchar(',');
-		_putchar(' ');
-		_putchar((k / 100) + '0');
-		_putchar(((k / 10) % 10) + '0');
-		_putchar((k % 10) + '0');
-	}
-}
-
