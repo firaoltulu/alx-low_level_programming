4fbe73e256e6ea75eada78c052a9dc0f5c275778
firaoltulu <firaoltulu5@gmail.com>
2023-05-23 23:11:38 -0700

    my first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - Entry function Test the
+* function positive_or_negative
+* author: firaol tulu lemma
+* Return: 0
+*/
+
+int main(void)
+{
+	int num;
+
+	num = 0;
+	positive_or_negative(num);
+	return (0);
+}
