19264835de97f5aa3fd0779a212406005452a2ba
firaoltulu <firaoltulu5@gmail.com>
2023-05-25 04:26:40 -0700

    my fourth commit

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -2,17 +2,13 @@
 #include <math.h>
 
 /**
- * LargestnumberPrimeFactor - finds and prints
- * the largest prime factor of the number 612852475143
+ * main - Entry Function
  *
- * @number: long long Integer type that accept
- * whos values is the one we trying to find the prime
- * factory
- *
- * Return: nothing (void)
+ * Return: 0
  */
-void LargestnumberPrimeFactor(long long int number)
+int main(void)
 {
+long long int number = 612852475143;
 long long int two = 2;
 long long int one = 0;
 
@@ -28,17 +24,7 @@ one = two;
 }
 two++;
 }
-printf("%lld\n", one);
-}
-
-/**
- * main - Entry Function
- *
- * Return: 0
- */
-int main(void)
-{
-long long int one = 612852475143;
-LargestnumberPrimeFactor(one);
+printf("%lld", one);
+printf("\n");
 return (0);
 }
