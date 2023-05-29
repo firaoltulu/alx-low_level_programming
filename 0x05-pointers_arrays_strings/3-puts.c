1d25b668c78cc086704ada803cb4f852b23fa060
firaoltulu <firaoltulu5@gmail.com>
2023-05-29 00:30:19 -0700

    my fourth commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string pointer that points to the variable
+ * that is passed to this function to print
+ * Return: nothing (void)
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		printf(*str++);
+	}
+	puts('\n');
+}
