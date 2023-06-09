77e9100cd1a3fa9c0d715e7de7ed73b49da43975
firaoltulu <firaoltulu5@gmail.com>
2023-06-09 05:46:49 -0700

    my twelve commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry function which prints the name of the program
+ * @argc: Integer that is passed to this function
+ * to represent number of arguments
+ * @argv: Char Pointer that points to the variable
+ * that is passed to this function to represent array
+ * of arguments
+ *
+ * Return: Integer 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", *argv);
+
+return (0);
+}
