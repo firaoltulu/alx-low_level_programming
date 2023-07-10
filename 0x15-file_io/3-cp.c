3b9d7c205f845132351a3e57bacc882077e81ca4
unknown <firaoltulu5@gmail.com>
2023-07-10 14:30:45 +0300

    my fifth commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,104 +2,131 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *firaol_create_buffer(char *file);
-void firaol_close_file(int fd);
+void fun_1(int one);
 
 /**
- * firaol_create_buffer - This Function Allocates 1024
- * bytes for a buffer.
- * @file: char pointer that points to The name
- * of the file buffer is storing chars for.
+ * fun_3 - THis Function checks that
+ * three was created and can be written to.
+ * @one: ssize_t that is passed to represent
+ * checks if true of false.
+ * @two: char pointer that points to a file name.
+ * @three: int that represent a file descriptor
+ * of file or -1.
+ * @four: int that represent a file descriptor
+ * of file, or -1
  *
- * Return: A Char pointer to the newly-allocated buffer.
+ * Return: Nothing (void)
  */
-char *firaol_create_buffer(char *file)
+void fun_3(ssize_t one, char *two, int three, int four)
 {
-	char *one;
-
-	one = malloc(sizeof(char) * 1024);
-
-	if (one == NULL)
+	if (one == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", two);
+		if (three != -1)
+			close(three);
+		if (four != -1)
+			close(four);
 		exit(99);
 	}
-	else
-	{
-		return (one);
-	}
 }
-
 /**
- * firaol_close_file - This Function Closes file descriptors.
- * @fd: int that represent The file descriptor to be closed.
+ * fun_4 - THis Functio checks that
+ * A file Pointer were closed properly.
+ * @one: Int That is Passed to Represent
+ * a checks if true or false.
+ * @two: int that represent a file descriptor
+ * of a file or -1.
+ *
+ * Return: Nothing (void).
  */
-void firaol_close_file(int fd)
+void fun_4(int one, int two)
 {
-	int two;
-
-	two = close(fd);
-
-	if (two == -1)
+	if (one == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", two);
 		exit(100);
 	}
 }
 
 /**
- * main - this Function Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * fun_2 - this function checks
+ * that file three exists and can be read.
+ * @one: size_t passed to represent the
+ * checks if true of false.
+ * @two: char pointer that points to
+ * the file name.
+ * @three: int that represent the file
+ * descriptor  of a file or -1.
+ * @four: int that represent the
+ * file descriptor of a file or -1.
+ *
+ * Return: Nothing (void).
+ */
+void fun_2(ssize_t one, char *two, int three, int four)
+{
+	if (one == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", two);
+		if (three != -1)
+			close(three);
+		if (four != -1)
+			close(four);
+		exit(98);
+	}
+}
+
+/**
+ * main - This Function Copies the
+ * content of a file to another file.
+ * @argc: int that represent the
+ * number of arguments passed.
+ * @argv: array of pointers to the arguments.
  *
  * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
-	int three, four, five, six;
-	char *seven;
+	int one, two, three, four;
+	ssize_t six;
+	ssize_t five;
+	char seven[1024];
+	mode_t eight;
 
-	if (argc != 3)
+	fun_1(argc);
+	one = open(argv[1], O_RDONLY);
+	fun_2((ssize_t)one, argv[1], -1, -1);
+	eight = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	two = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, eight);
+	fun_3((ssize_t)two, argv[2], one, -1);
+	five = 1024;
+	while (five == 1024)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_three file_to\n");
-		exit(97);
+		five = read(one, seven, 1024);
+		fun_2(five, argv[1], one, two);
+		six = write(two, seven, five);
+		if (six != five)
+			six = -1;
+		fun_3(six, argv[2], one, two);
 	}
-	else
+	three = close(two);
+	four = close(one);
+	fun_4(three, two);
+	fun_4(four, one);
+	return (0);
+}
+/**
+ * fun_1 - This Function checks
+ * for the correct number of arguments.
+ * @one: int that represent the number
+ * of arguments.
+ *
+ * Return: Nothing (void)
+ */
+void fun_1(int one)
+{
+	if (one != 3)
 	{
-		seven = firaol_create_buffer(argv[2]);
-		three = open(argv[1], O_RDONLY);
-		five = read(three, seven, 1024);
-		four = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-		do {
-			if (three == -1 || five == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't read three file %s\n", argv[1]);
-				free(seven);
-				exit(98);
-			}
-			else
-			{
-				six = write(four, seven, five);
-				if (four == -1 || six == -1)
-				{
-					dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-					free(seven);
-					exit(99);
-				}
-				else
-				{
-					five = read(three, seven, 1024);
-					four = open(argv[2], O_WRONLY | O_APPEND);
-				}
-			}
-
-		} while (five > 0);
-
-		free(seven);
-		firaol_close_file(three);
-		firaol_close_file(four);
-
-		return (0);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 }
